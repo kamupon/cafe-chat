fe30f8d58c267109820b29b94423a0b5e1e9d15e
honda <yhonda1001@gmail.com>
2016-01-07 20:53:30 +0900

    packages指定が誤っていた箇所を修正した.

--- server/src/main/java/net/kamupon/chat/server/MyApplication.java
+++ server/src/main/java/net/kamupon/chat/server/MyApplication.java
@@ -14,7 +14,7 @@ import org.glassfish.jersey.server.mvc.jsp.JspMvcFeature;
 public class MyApplication extends ResourceConfig {
     
     public MyApplication(){
-        packages("example.server");
+        packages("net.kamupon.chat.server");
         
         register(JspMvcFeature.class);
         property(JspMvcFeature.TEMPLATE_BASE_PATH, "/WEB-INF/view");
